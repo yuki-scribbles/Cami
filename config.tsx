a0aba6ad3c7189a9f6ccf5dbb1a290b4c2c5263d
yuki-scribbles <trina0dang@gmail.com>
2024-12-07 13:01:54 -0600

    initial commit

---
+++ config.tsx
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD4GPtrN1J6fmAvOE0aoOManp1ySO3YdBM",
+  authDomain: "cami-9ed69.firebaseapp.com",
+  databaseURL: "https://cami-9ed69-default-rtdb.firebaseio.com",
+  projectId: "cami-9ed69",
+  storageBucket: "cami-9ed69.firebasestorage.app",
+  messagingSenderId: "546492429326",
+  appId: "1:546492429326:web:fd536d63f6f076d247785f",
+  measurementId: "G-VHT606X84T"
+};
